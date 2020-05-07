6bf24843e20c268babd427f0c5243e5499b1653d
Timits <will4f21@yahoo.com>
2020-05-07 02:01:51 -0700

    first commit

--- DB/Connection.js
+++ DB/Connection.js
@@ -1,7 +1,5 @@
 const mongoose = require('mongoose');
 
-// const URI  = "mongodb+srv://dbUser:dbUser@cluster0-ytn8c.mongodb.net/test?retryWrites=true&w=majority";
-
 const connectDB = async() => {
     await mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0-ytn8c.mongodb.net/test?retryWrites=true&w=majority`, { useUnifiedTopology: true, useNewUrlParser: true });
     console.log('db connected!')
